f27940d3053fb5fc2fd5f260275067006c58efb8
Stefen Sumla <stefen.sumla1@gmail.com>
2022-02-13 15:11:57 +0200

    First commit

---
+++ Mixed messages.js
@@ -0,0 +1,13 @@
+let adjectives=[];
+let nouns=[];
+let verbs=[];
+let places=[];
+let words=[];
+
+function randomNumber(){
+    return Math.floor(Math.random()*20);
+}
+
+console.log(
+    adjectives[randomeNumber]
+)
